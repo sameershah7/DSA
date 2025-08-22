97505db9969033909c065a9ad2951062620d47b9
Sameer Shah <shahkhanpc8925@gmail.com>
2025-08-22 21:45:20 +0500

    Insert new node at front and last diffines funtions in linked list

---
+++ 13. Liked List/SinglyLinkedList.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Definition of a Node in a singly linked list
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+// Function to traverse and print the singly linked list
+void traverseList(Node *head)
+{
+    while (head != nullptr)
+    {
+        // Printing data of the current node
+        cout << head->data << " " << endl;
+
+        // Move to the next node
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Checks whether key is present in linked list
+bool searchKey(Node *head, int key)
+{
+    Node *curr = head;
+    if (curr == NULL)
+        return false;
+    if (curr->data == key)
+        return true;
+    return searchKey(curr->next, key);
+}
+
+// Function to insert a new node at the beginning of the list
+Node *insertAtFront(Node *head, int new_data)
+{
+    Node *new_node = new Node(new_data);
+    new_node->next = head;
+    return new_node;
+}
+
+Node *insertAtEnd(Node *head, int new_data)
+{
+    Node *new_node = new Node(new_data);
+    
+    if (head == nullptr) return new_node;
+
+    Node *last = head;
+
+    while (last->next != nullptr)
+        last->next = last;
+
+    last->next = new_node;
+    
+    return head;
+
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    head->next = new Node(12);
+    head->next->next = new Node(14);
+    head->next->next->next = new Node(16);
+    head->next->next->next->next = new Node(18);
+
+    int key = 10;
+    int insVal = 12;
+
+    cout <<"Seachin key: "<< searchKey(head, key);
+    head = insertAtFront(head, insVal);
+    head = insertAtEnd(head, insVal);
+    traverseList(head);
+
+    return 0;
+}
