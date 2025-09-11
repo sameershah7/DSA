f1a0e7c9901489c6b6a50719d143c0dc169ca732
Sameer Shah <shahkhanpc8925@gmail.com>
2025-09-11 11:44:13 +0500

    Representation of Undirected Graph as Adjacency list

---
+++ 17. Graph/directedRep.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to add an edge between two vertices
+void addEdge(vector<vector<int>> &adj, int i, int j)
+{
+    adj[i].push_back(j);
+    adj[j].push_back(i); // Undirected
+}
+
+void displayAdjList(const vector<vector<int>> &adj)
+{
+    for (int i = 0; i < adj.size(); i++)
+    {
+        cout << i << ": "; // Print the vertex
+        for (int j : adj[i])
+            cout << j << " "; // Print its adjacent
+        cout << endl;
+    }
+}
+
+int main()
+{
+    // Create a graph with 4 vertices and no edges
+    int V = 4;
+    vector<vector<int>> adj(V);
+
+    // Now add edges one by one
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 2, 3);
+
+    cout << "Adjacency List Representation:" << endl;
+    displayAdjList(adj);
+
+    return 0;
+}
