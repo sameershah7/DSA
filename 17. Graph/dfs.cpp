4f4b5ff65b5691051e8b91dfad52358f5d493bda
Sameer Shah <shahkhanpc8925@gmail.com>
2025-09-14 17:01:27 +0500

    DFS for Complete Traversal of Disconnected Undirected Graph

--- 17. Graph/dfs.cpp
+++ 17. Graph/dfs.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-
 using namespace std;
 
 // Recursive function for DFS traversal
@@ -10,9 +8,10 @@ void dfsRec(vector<vector<int>> &adj, vector<bool> &visited, int s, vector<int> 
     visited[s] = true;
     res.push_back(s);
     // Recursively visit all adjacent vertices that are not visited
-    for(int i: adj[s]){
-        if(visited[i] == false)
-            dfsRec(adj, visited, i , res);
+    for (int i : adj[s])
+    {
+        if (visited[i] == false)
+            dfsRec(adj, visited, i, res);
     }
 }
 
@@ -21,8 +20,18 @@ vector<int> DFS(vector<vector<int>> &adj)
 {
     vector<bool> visited(adj.size(), false);
     vector<int> res;
-    dfsRec(adj, visited, 0, res);
+    // Don't need to through all vertices for connected graph
+    // dfsRec(adj, visited, 0, res);
 
+    // Loop through all vertices to handle disconnected graph
+    for (int i = 0; i < adj.size(); i++)
+    {
+        if (visited[i] == false)
+        {
+            // If vertex i has not been visited, perform DFS from it
+            dfsRec(adj, visited, i, res);
+        }
+    }
     return res;
 }
 
@@ -39,13 +48,20 @@ int main()
     vector<vector<int>> adj(V);
 
     // Add edges
-    vector<vector<int>> edges = {{1, 2}, {1, 0}, {2, 0}, {2, 3}, {2, 4}};
+    // Connected edges
+    // vector<vector<int>> edges = {{0, 1}, {1, 2}, {2, 3}, {3, 0}};
+
+    // Disconnected edges
+    vector<vector<int>> edges = {{0, 1}, {2, 3}, {3, 4}};
+
     for (auto &e : edges)
         addEdge(adj, e[0], e[1]);
 
-    // Starting vertex for DFS
-    vector<int> res = DFS(adj); // Perform DFS starting from the source verte 0;
+    // Perform DFS starting from the source vertex 0;
+    vector<int> res = DFS(adj);
 
-    for (int i = 0; i < V; i++)
-        cout << res[i] << " ";
+    for (auto i : res)
+        cout << i << " ";
+
+    return 0;
 }
